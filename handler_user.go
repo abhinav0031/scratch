5a376fb8c8af4910b5cdfe99470dbfbd4ed59450
abhinav0031 <abhinav16100@gmail.com>
2024-03-20 01:17:47 +0530

    added api_key column and GetUserByAPIKey and auth,go

--- handler_user.go
+++ handler_user.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/abhinav0031/scratch/internal/auth"
 	"github.com/abhinav0031/scratch/internal/database"
 	"github.com/google/uuid"
 )
@@ -17,21 +18,37 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
-	err:=decoder.Decode(&params)
+	err := decoder.Decode(&params)
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Error parsing JSON: %v", err))
 		return
 	}
-	user,err:=apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
-		ID:uuid.New(),
-		CreatedAt:time.Now().UTC(),
-		UpdatedAt:time.Now().UTC(),
-		Name:params.Name,
+	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
 	})
-	if err!=nil{
-		respondWithError(w,400,fmt.Sprintf("Couldn't create user %v",err))
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't create user %v", err))
 		return
 	}
 
-	respondWthJson(w,200,user)
+	respondWthJson(w, 200, databaseUserToUser(user))
+}
+
+func (cfg *apiConfig) handleUsersGet(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find api key")
+		return
+	}
+
+	user, err := cfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't get user")
+		return
+	}
+
+	respondWthJson(w, http.StatusOK, databaseUserToUser(user))
 }
--- main.go
+++ main.go
@@ -14,36 +14,35 @@ import (
 	_ "github.com/lib/pq"
 )
 
-type apiConfig struct{
+type apiConfig struct {
 	DB *database.Queries
 }
 
-func main(){
+func main() {
 	godotenv.Load(".env")
-	portString:=os.Getenv("PORT")
+	portString := os.Getenv("PORT")
 
-	if portString==""{
+	if portString == "" {
 		log.Fatal("Port is not found in the env")
 	}
 
-	dbURL:=os.Getenv("DB_URL")
+	dbURL := os.Getenv("DB_URL")
 
-	if dbURL==""{
+	if dbURL == "" {
 		log.Fatal("DB_URL is not found in the env")
 	}
 
-	conn,err:=sql.Open("postgres",dbURL)
-	
+	conn, err := sql.Open("postgres", dbURL)
 
-	if err!=nil{
+	if err != nil {
 		log.Fatal("Can't conenct tp database")
 	}
-    queries:=database.New(conn)
-	
-	apiCfg:=apiConfig{
-		DB:queries,
+	queries := database.New(conn)
+
+	apiCfg := apiConfig{
+		DB: queries,
 	}
-	router:=chi.NewRouter();
+	router := chi.NewRouter()
 	router.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"https://*", "http://*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
@@ -53,20 +52,21 @@ func main(){
 		MaxAge:           300,
 	}))
 
-	v1Router:=chi.NewRouter()
+	v1Router := chi.NewRouter()
 
-	v1Router.Get("/healthz",handlerReadiness)
-	v1Router.Get("/err",handlerErr)
-	v1Router.Post("/users",apiCfg.handlerCreateUser)
-	router.Mount("/v1",v1Router)
+	v1Router.Get("/health", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
+	v1Router.Get("/users", apiCfg.handleUsersGet)
+	router.Mount("/v1", v1Router)
 
-	srv:=&http.Server{
-		Handler:router,
-		Addr:":"+portString,
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
 	}
-	log.Printf("Server starting on port %v",portString)
-	err=srv.ListenAndServe()
-	if err!=nil{
+	log.Printf("Server starting on port %v", portString)
+	err = srv.ListenAndServe()
+	if err != nil {
 		log.Fatal(err)
 	}
 }
